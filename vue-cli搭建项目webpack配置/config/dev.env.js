e7ccec72722ae670f97d5f7cc5b8790da4f603fb
剑恋歌 <1440067266@qq.com>
2021-07-05 14:54:39 +0800

    20210705

---
+++ vue-cli搭建项目webpack配置/config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://localhost:8888"',
+  FILE_APT: '"http://localhost:8888"'
+})
---
+++ vue-cli搭建项目webpack配置/config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://localhost:8888"',
+  FILE_APT: '"http://localhost:8888"'
+}
